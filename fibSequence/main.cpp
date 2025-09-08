4e5942b4c31197de9f98d30d66fd8b975420bb26
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-09-08 10:18:39 -0500

    Initial Commit

---
+++ fibSequence/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fibSequence
+//
+//  Created by Gabriela Avila on 9/8/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
