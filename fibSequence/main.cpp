7b0a0a71541868a92f6f636046d2b7277be47b58
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-09-09 10:10:28 -0500

    p1

--- fibSequence/main.cpp
+++ fibSequence/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+using namespace std;
+int fib_seq(int n)
+{
+    static int count = 0;
+    count ++;
+    cout << count << " " << endl;
+    if (n > 2)
+    {
+        return fib_seq(n-1) + fib_seq(n-2);
+    }else if (n == 0)
+    {
+        return 0;
+    }else {
+        return 1;
+    }
+    return 0;
+}
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout << "\n " << fib_seq(10) << endl;
     return 0;
 }
